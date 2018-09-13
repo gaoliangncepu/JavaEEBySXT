30944268783a18fde71628edb3a37629c8de860a
301_GaoL <gl_ncepu@163.com>
2018-09-13 12:50:53 +0800

    update LoginService

--- LoginService/src/com/learn/dao/UserDao.java
+++ LoginService/src/com/learn/dao/UserDao.java
@@ -1,12 +1,3 @@
-<<<<<<< HEAD
-package com.learn.dao;
-
-import com.learn.pojo.User;
-
-public interface UserDao {
-	User checkLoginMsg(String name, String pwd);
-}
-=======
 package com.learn.dao;
 
 import java.util.List;
@@ -16,4 +7,3 @@ import com.learn.pojo.User;
 public interface UserDao {
 	List<User> checkUser(String name, String pwd);
 }
->>>>>>> 7855f92e285d962128c4ce3585e9bbfdc96665df
--- LoginService/src/com/learn/servlet/PageServlet.java
+++ LoginService/src/com/learn/servlet/PageServlet.java
@@ -1,36 +1,3 @@
-<<<<<<< HEAD
-package com.learn.servlet;
-
-import java.io.IOException;
-
-import javax.servlet.ServletException;
-import javax.servlet.http.HttpServlet;
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
-
-/**
- * Servlet µÇÂ¼Ò³Ãæ
- */
-public class PageServlet extends HttpServlet {
-	@Override
-	protected void service(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-		resp.setContentType("text/html;charset=utf-8");
-
-		resp.getWriter().write("<form action='Login' method='GET'>");
-		resp.getWriter().write("<html>");
-		resp.getWriter().write("<head>");
-		resp.getWriter().write("</head>");
-		resp.getWriter().write("<body>");
-		resp.getWriter().write("µÇÂ¼Ãû:<input type='text' name='uname' value=''><br />");
-		resp.getWriter().write("ÃÜÂë:<input type='password' name='pwd' value=''><br />");
-		resp.getWriter().write("<input type='submit' value='µÇÂ¼'>");
-		resp.getWriter().write("</body>");
-		resp.getWriter().write("</html>");
-		resp.getWriter().write("</form>");
-	}
-
-}
-=======
 package com.learn.servlet;
 
 import java.io.IOException;
@@ -71,4 +38,3 @@ public class PageServlet extends HttpServlet {
 		
 	}
 }
->>>>>>> 7855f92e285d962128c4ce3585e9bbfdc96665df
