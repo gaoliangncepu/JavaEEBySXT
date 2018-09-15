51b509740163b90c23c5a762153eba6d3d3a8905
Home_GaoL <gl_ncepu@163.com>
2018-09-15 21:05:35 +0800

    learn

--- ServletContext/src/com/learn/servlet/ServletContextServlet.java
+++ ServletContext/src/com/learn/servlet/ServletContextServlet.java
@@ -13,25 +13,28 @@ import javax.servlet.http.HttpServletResponse;
  * 	解决：
  * 		不同用户的不同请求之间的数据共享问题。
  * 	方式：
- * 		在服务器端创建用于存放用户数据的ServletContext对象，此对象是一个全局单例对象，一个项目的生命周期中只能创建一个
- * 	ServletContext对象，且ServletContext对象的生命周期是从项目启动到项目停止。所有用户均有访问和修改ServletContext
- * 	对象信息的权限。
+ * 		在服务器端创建用于存放用户数据的ServletContext对象，此对象是一个全局单例对象，与整个项目同寿命。
+ * 	一个项目的生命周期中只能创建一个ServletContext对象，且ServletContext对象的生命周期是从项目启动到项目停止。
+ * 	所有用户均有访问和修改ServletContext对象信息的权限。
  * 	使用：
  * 		// 获取ServletContext对象的三种方式
 		ServletContext sc1 = this.getServletContext();
 		ServletContext sc2 = this.getServletConfig().getServletContext();
 		ServletContext sc3 = req.getServletContext();
 		ServletContext sc4 = req.getSession().getServletContext();
+		// 设置存储信息
+		sc.setAttribute(String name, Object value);
 		// 获取存储的信息
-		String value = (String) sc.getAttribute(String name);
+		String value = (String) sc.getAttribute(String name);//默认返回Object类型，不存在时返回null
 		Enumeration<String> enumeration = sc.getAttributeNames();
-		// 获取webroot（根目录）下的资源文件的绝对路径
-		String path = sc.getRealPath(String relativePath);
-		// 获取项目公共配置参数
+		// 获取项目全局配置参数
 		String value = sc.getInitParameter(String name);
 		Enumeration<String> initParameterNames = sc.getInitParameterNames();
+		// 获取webroot（根目录）下的资源文件的绝对路径
+		String path = sc.getRealPath(String relativePath);
 		// 获取webroot(根目录)下资源文件的输入流
 		InputStream resourceAsStream = sc.getResourceAsStream(String relativePath);
+		注意：class文件的流对象必须使用类加载器来获取。
  *
  */
 public class ServletContextServlet extends HttpServlet{
